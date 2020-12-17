d2634445437c959e8acb96e8778c362759e7cc60
Margaret Ralston <pandahart@gmail.com>
2020-12-17 14:32:38 -0800

    we're now checking to see if an event for the time has already been saved and if so simply updating the text. the schedule is then saved to local storage. also added a loadEvents function to fill in the schedule on page load/refresh.

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ $(document).ready(function() {
 
 	var hour = dayjs().format('HH00');
 
+	loadEvents();
+
 	$(".schedule").each(function() {
 		if( $(this).attr("data-hour") == hour ) {
 			$(this).parent().addClass("present");
@@ -31,10 +33,51 @@ $(document).ready(function() {
 			'date': date
 		}
 
-		schedule.push(event);
-
-		localStorage.setItem('planner-schedule', JSON.stringify(schedule));
+		saveEvent(event);
 		
 	});
+
+	function saveEvent(event) {
+		var foundMatchingEvent = false;
+		// loop through everything in the schedule array
+		for(var i = 0; i < schedule.length; i++) {
+			// if there's already an event for the time being saved,
+			// overwrite the previous text instead of saving a new 'entry'
+			if( schedule[i].time === event.time ) {
+				schedule[i].text = event.text;
+				foundMatchingEvent = true;
+			}
+		}
+
+		// if no matching event was found, just push the entire event to
+		// the schedule array
+		if(!foundMatchingEvent) {
+			schedule.push(event);
+		}
+
+		console.log(schedule);
+
+		// save to local storage (last step)
+		localStorage.setItem('planner-schedule', JSON.stringify(schedule));
+	}
+
+	function loadEvents() {
+		// make sure the schedule array actually has something in it
+		if( schedule.length > 0 ) {
+			// loop through the array
+			for(var i = 0; i < schedule.length; i++) {
+				// make sure the date matches before entering the text
+				if( schedule[i].date === date ) {
+					$(`#schedule-${schedule[i].time}`).val(schedule[i].text);
+
+				} else {
+					// if the event's date does NOT match *today's date*
+					// let's remove it from the schedule
+					schedule.splice(i, 1);
+
+				}
+			}
+		}
+	}
 
 });
