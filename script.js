2c0e0b73146443c8ca9e137c25b8a62c03eaedb9
Margaret Ralston <pandahart@gmail.com>
2020-12-17 13:24:44 -0800

    now getting the current hour, in 24-hour time.

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
 	var currentDay = dayjs().format('MMMM DD, YYYY');
 
 	$("#currentDay").text(currentDay);
+
+	var hour = dayjs().format('HH00');
+	console.log(hour);
 });
